982ce19970f5e1300a4487bdf3b903c7797f7f67
greatsilas23 <nsilasn@gmail.com>
2022-02-23 17:36:04 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+/*
+    Date created: 23 Feb
+    Daate modified: 23 Feb
+*/
+//express
 const express = require('express')
+//parse variable before arithmetic operations
+const bodyParser = require('body-parser')
+//instance
+const app = express()
+/*HTTP
+    method: post
+    param: req, res, route
+    output: monthlyAllowance
+*/
+app.post('/', (req,res) => {
+//0 or more inputs
+    let varAllowance = Number(req.body.varAllowance)
+    let varB = Number(req.body.varB)
+    res.status(204).send = varAllowance + varB
+})
+//1 or more output
+app.listen(`listening on port 4000`, process.env.PORT || 4000)
