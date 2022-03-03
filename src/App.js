abb56f64b703e26e02c26020620e509ae0c11415
greatsilas23 <nsilasn@gmail.com>
2022-03-03 22:23:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstAidForm from './FirstAidForm'
+
+
 
 class App extends Component {
   render() {
     return(
         <div>
-           <table>
-                <tr>
-                    <td>
-                        <p>0</p>
-                    </td>
-                    <td>
-                        <p>1</p>
-                    </td>
-                    <td>
-                        <p>0</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <p>0</p>
-                    </td>
-                    <td>
-                        <p>1</p>
-                    </td>
-                    <td>
-                        <p>0</p>
-                    </td>
-                </tr>
-        </div>
+
+
+
+                        <FirstAidForm />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+	
+
+
+		</div>
     )
   }
 }
---
+++ src/FirstAidForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const FirstAidForm = () => {
+	return(
+
+		<div>
+			<form>
+
+
+				<label for="nameInput"> 
+					Name
+
+				</label>
+
+
+
+							<input id="nameInput" type="text" />
+
+
+
+
+							<label>
+								Age
+
+							</label>
+
+							<input type="text" />
+
+
+							<input type="submit"/>
+		</form>
+		</div>)
+}
+export default FirstAidForm
+
+
