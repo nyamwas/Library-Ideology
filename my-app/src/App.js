4f328ecd3432cb8b582c47905861af50a541789f
greatsilas23 <nsilasn@gmail.com>
2022-05-19 12:03:10 +0300

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react'
+
+
+
+
+import Footer from './components/Footer.js'
+import Navigation from './components/Navigation.js' 
+
+import Card from './comopnents/Card.js'
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state({
+
+      navigations: [
+        {content: "home"},
+        {contenti: "about"},
+        {content: "feedback"}
+     ],
+     users: [
+        {contenti: "admin"},
+        {content: "client"},
+        {content: "maintenance"}
+     ],
+
+     footers: [
+        {content: "facebook"},
+        {content: "github"},
+
+     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+    })
+  }
+  componentDidMount(){
+
+   axios({
+
+   
+
+      url: "",
+
+      method: "get", 
+
+      withCredentials: true,
+
+      header: {
+       Control-Access-Control-Origin: "*",
+       Content-Type: "application/json", 
+
+       Accepted: "application/json"
+
+      }
+      }).then((response) => {
+
+
+         if( response && response.data){
+             this.setState({users: response.data})
+         }
+
+      }).catch((err) => {
+
+         if(err) throw err
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      })
+
+      }
+
+       
+
+  render() {
+
+   return (
+    <div class="container">
+
+       <header class="header">
+           <p>health-inventory-app</p>
+
+
+
+           {this.state.navigations.map((navigation) => {<Navigation navigationContent={navigation.content}/>})}  
+       </header>
+       <section class="body">
+
+
+
+           {this.state.users.map((user) => <Card cardContent={user.content} />)} 
+      </section>
+      <footer class="footer">
+
+
+        {this.state.footers.map((footer) => {<Footer footerContent={footer.content}/>})}
+        <span> greatsilas23 </span> &copy; <span> 2021 </span>
+      </footer>
     </div>
-  );
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  )
+
+ }
+
+}
 export default App;
