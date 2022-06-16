2fb99333ee328c2ecbe425c0c0a61f8ea1cd66fe
greatsilas23 <greatsilas23@gmail.com>
2022-06-16 12:12:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import axios from "axios"
 
 
 class App extends Component {
@@ -8,56 +8,51 @@ class App extends Component {
     this.state = {
       showHome: false,
       showLogin: true,
+      validLogin: false,
       users: [],
       items: []
+
    }
   }
 
+
+  handleLogin = (userName, userPass ) => {
+   for(let i = 0; i < this.state.users.length; i++){
+    if(userName === this.state.users[i].userName && userPass === this.state.users[i].userPass){
+      this.setState({validLogin : true })
+    } else {
+      this.setState({ validLogin: false})
+    }
+  }
+  }
+
+  componentDidMount = () => {
+    axios({
+      url: "",
+      method: "post",
+      header: {
+        Control-Access-Content-Origin: "apllication/json",
+        Content-Type: "application/json",
+        withCredentials: true
+      }
+    }).then((response) => {
+      this.setState({users: response.data})
+    }).catch((err) => {
+      if (err) throw err
+
+    })
+  }
   render(){
   return (
     <div className="w-100 h-100 m-auto bg-info">
-
-       {this.state.showLogin &&
-           <section className="w-30 h-30 m-auto bg-warning">
-
-
-
-             <form
-                 action="http://localhost:4000/login"
-                 method="post"
-             >
-               <label>Username:</label>
-               <input type="text" id="userName" name="userName" placeholder="example@gmail.com"/><br/>
-               <label>Password:</label>
-               <input type="password" id="userPass" name="userPass" placeholder="****"/><br/>
-               <input type="submit" value="LOG-IN"/><br/>
-               <label>Remember Me </label><input type="checkbox"/><br/>
-               <span>Not a registered user? Sign up</span> <a href="register.html">here</a>
-             </form>
-           </section>
-       }
-       {this.state.showHome &&
-           <section>
-             <a> Home </a>
-             <a> About </a>
-             <a> Contacts </a>
-             <a> Cart </a>
-             <a> Billing </a>
-             <a> Profile </a><br/>
-
-             <span> greatsilas23 </span> &copy;
-           </section>
-       }
-      {this.state.showCheckout &&
-          <section>
-            <form action="http://localhost:4000/checkout" method="post">
-              <input type="file">
-              Id: <input type="text" placeholder="ID001"/>
-              Name: <input type="text" placeholder="Falafel" />
-              Price: <input type="number" placeholder="Ksh. 100"/>
-              <input type="submit"/>
-            </form>
-          </section>
+      {this.state.showRegister &&
+        <div className={"w-50 h-50 m-5 p-5 bg-info"}>
+          <form onSubmit={() => handleLogin} >
+            Email: <input type="email"/>
+            Password: <input type="password"/>
+            <input type="submit"/>
+          </form>
+        </div>
       }
     </div>
   )
