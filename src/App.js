3219d413df0b7262de610ccd72abc6c2734113b9
greatsilas23 <greatsilas23@gmail.com>
2022-06-19 14:10:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import axios from "axios"
-
-
+import './App.css'
 class App extends Component {
   constructor(props){
     super(props)
@@ -15,46 +13,63 @@ class App extends Component {
    }
   }
 
-
-  handleLogin = (userName, userPass ) => {
-   for(let i = 0; i < this.state.users.length; i++){
-    if(userName === this.state.users[i].userName && userPass === this.state.users[i].userPass){
-      this.setState({validLogin : true })
-    } else {
-      this.setState({ validLogin: false})
-    }
-  }
-  }
-
-  componentDidMount = () => {
-    axios({
-      url: "",
-      method: "post",
-      header: {
-        Control-Access-Content-Origin: "apllication/json",
-        Content-Type: "application/json",
-        withCredentials: true
-      }
-    }).then((response) => {
-      this.setState({users: response.data})
-    }).catch((err) => {
-      if (err) throw err
-
-    })
-  }
   render(){
   return (
-    <div className="w-100 h-100 m-auto bg-info">
-      {this.state.showRegister &&
-        <div className={"w-50 h-50 m-5 p-5 bg-info"}>
-          <form onSubmit={() => handleLogin} >
-            Email: <input type="email"/>
-            Password: <input type="password"/>
-            <input type="submit"/>
-          </form>
-        </div>
-      }
-    </div>
+<div>
+	<section className="popup hide">
+		<div className="blur">
+		</div>
+		<div className="card">
+			<img  height="50" width="50" alt="item"/>
+			<h3>Ksh. 100</h3> 
+			<form action="http://loalchost:4000/billing" method="post">
+		  		<input type="image"  width="30" height="30" alt="buy" />
+			</form>
+		</div>
+	</section>
+	<header>
+		<img  width="40" height="40" alt="logo"/>
+		<section className="navigation">
+			<a href="index.html"><img  width="40" height="40" alt="login"/></a>
+			<a href="checkout.html"><img  width="40" height="40" alt="cart"/></a>
+			<a href="billing.html"><img  width="40" height="40" alt="bill "/></a>
+			<a href="login.html"><img  width="40" height="40" alt="login"/></a>
+		</section>
+	</header> 
+	<div className="container">
+		<section className="blur">  
+			<section className="menu hide">
+				<img  width="40" height="40" alt="vegetables" />
+				<h2>Vegetables</h2>  
+				<form action="http://localhost:4000/checkout" method="post">
+					<img alt="logo" height="50" width="50" /> 
+					<hr/>
+					<input type="number" value="1" className="input" id="qty" name="qty" />
+					<button className="button">
+						<img width="60" height="30" />
+					</button>
+				</form>
+			</section>
+		</section>
+		<section className="item-container">
+			<div className="card">
+				<img height="50" width="50" alt="item"/>
+				<h3>Ksh. 100</h3> 
+				<form action="http://loalchost:4000/billing" method="post">
+			  		<input type="image" width="30" height="30" alt="buy" />
+				</form>
+			</div>
+			<div className="card">
+				<img height="50"  width="50" alt="item"/>
+				<h3>Ksh. 200</h3>
+				<input type="image" className="button"  width="40" height="40"/>
+			</div>
+		</section>
+	</div>
+	<footer>
+		<span> greatsilas23 </span> &copy; <span> 2021</span>
+	</footer>
+</div>
   )
   }
 }
