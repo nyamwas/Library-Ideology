16499b8ac63c73d6e91f2f90a00e3764a895d6b6
greatsilas23 <greatsilas23@gmail.com>
2022-06-25 11:52:04 +0300

    first commit

--- App.js
+++ App.js
@@ -1,36 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { Client } = require('pg')
 const port = process.env.PORT || 4000
 const app = express()
-const client = new Client({
-	host: 'localhost',
-	user: 'postgres',
-	password: 'secret',
-	database: 'postgres',
-	port: '5432'
-})
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 
-
-app.post('/login', function(req, res) {
-	let userName = req.body.userName
-	console.log(`${userName}`)
-	res.sendStatus(204)
-})
 app.post('/checkout', (req,res) => {
-	let itemName = req.body.itemName
-	let itemPrice = req.body.itemPrice
-	client.query(`INSERT INTO checkout (id, name, price) VALUES ('${Math.floor(Math.random())}','${itemName}', '${itemPrice}')`, (err, result, fields) => {
-		if (err) throw err
-		res.sendStatus(204)
-	})
+	let item = req.body
 	res.json()
-})
-
-client.connect((err) => {
-	if (err) throw err
-	console.log(`connected!`)
+	res.sendStatus(204)
 })
 
 
--- src/App.js
+++ src/App.js
@@ -4,65 +4,52 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      showHome: false,
-      showLogin: true,
-      validLogin: false,
-      users: [],
-      items: []
-
-   }
+      items: [
+		
+		{name: "Jute Mallow"},
+    		{name: "Avocado"}
+	]
+    }
+    function handleChange(e)  {
+	e.target.value ? this.setState({ items: this.state.items.push(e.target.value)}) : console.log(`enter value`) 
+    }
+    function handleCart(e) {
+	e.preventDefault()
+	const { itemDescription } = this.state.items
+	const { cartItem } = fetch("http://localhost:4000/checkout", {
+		method: "POST",
+		body: JSON.stringify(itemDescription),
+ 		headers: {
+			"Content-Type": "application/json"			
+		}
+	})
+	console.log(`Added to cart`)
+    }
   }
-	componentDidMount(){
-	 	const item  = fetch("http://localhost:4000/checkout")
-		const response = item.json()
-		this.setState({ items: response})
-	}
+
   render(){
   return (
-<div>
-	<header>
-		<img  width="40" height="40" alt="logo"/>
-		<section className="navigation">
-			<a href="index.html"><img  width="40" height="40" alt="login"/></a>
-			<a href="checkout.html"><img  width="40" height="40" alt="cart"/></a>
-			<a href="billing.html"><img  width="40" height="40" alt="bill "/></a>
-			<a href="login.html"><img  width="40" height="40" alt="login"/></a>
-		</section>
-	</header> 
-	<div className="container">
-		<section className="blur">  
-			<section className="menu hide">
-				<img  width="40" height="40" alt="vegetables" />
-				<h2>Vegetables</h2>  
-				<form onSubmit={handleAddToCart} method="post">
-					<img alt="logo" height="50" width="50" /> 
-					<hr/>
-					<input type="number" value="1" className="input" id="qty" name="qty" />
-					<button className="button">
-						<img width="60" height="30" />
-					</button>
-				</form>
+	<div>
+		<div className="container">
+			<p>On Sale</p>
+			<section className="item-container">
+					<div className="card">
+						<form onSubmit={this.handleCart}>
+							<input type="text" onChange={this.handleChange}/>
+							<input type="submit"/>
+ 						</form>
+					</div>
 			</section>
-		</section>
-		<section className="item-container">
-			<div className="card">
-				<img height="50" width="50" alt="item"/>
-				<h3>Ksh. 100</h3> 
-				<form action="http://loalchost:4000/billing" method="post">
-			  		<input type="image" width="30" height="30" alt="buy" />
-				</form>
-			</div>
-			<div className="card">
-				<img height="50"  width="50" alt="item"/>
-				<h3>Ksh. 200</h3>
-				<input type="image" className="button"  width="40" height="40"/>
-			</div>
-		</section>
+			<section className="card">
+			{this.state.items.map((item) =>
+				<p>{item.name}</p>
+			)}
+			</section>
+		</div>
+		<footer>
+			<span> greatsilas23 </span> &copy; <span> 2021</span>
+		</footer>
 	</div>
-	<footer>
-		<span> greatsilas23 </span> &copy; <span> 2021</span>
-	</footer>
-</div>
   )
   }
 }
