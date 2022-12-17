4628c755352e3f26c51cd21592e3c5d82ca73f32
nyamwas <greatsilas23@gmail.com>
2022-12-17 20:12:21 +0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 /* Date created: 12th
-   Date created: 12th
+
+
+   Date modified: 17th
 */
-class Index{
-  constructor(new Array(x), new Object(y)){
-    this.x = x
-    this.y = y
-  }
-}
+
+
+
+
+
+const http = require('http')
+const url = require('url')
+
+const bodyParser = require('body-parser')
+
+
+
+
+
+
+http.createServer("."+ bodyParser.parse(url.params).pathname, (req, res) => {
+ res.end('Hello')
+}).listen(4000)
+
+
+
+     
 
